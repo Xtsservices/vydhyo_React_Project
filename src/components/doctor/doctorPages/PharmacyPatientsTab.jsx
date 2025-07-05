fd47fdbc316b6ae1b71464c1468c847aaf5ce8a9
pavansannalaXTS <pavansannala@xtsservices.com>
2025-07-05 14:38:47 +0530

    all

--- src/components/doctor/doctorPages/PharmacyPatientsTab.jsx
+++ src/components/doctor/doctorPages/PharmacyPatientsTab.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { 
   Table, 
   Button, 
@@ -128,7 +128,7 @@ const PatientsTab = () => {
 
   useEffect(() => {
       if (doctorId && user) {
-        getAllTestsPatientsByDoctorID();
+        // getAllTestsPatientsByDoctorID();
       }
     }, [user, doctorId]);
 
