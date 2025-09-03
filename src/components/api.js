74e01b94beac7b5340afc7243efc3583d43a0292
Mkp991203 <manikumarkuyyeti@gmail.com>
2025-09-03 12:18:00 +0530

    ip

--- src/components/api.js
+++ src/components/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 // Base API URL
-// const API_BASE_URL = "http://192.168.0.105:3000";
+// const API_BASE_URL = "http://localhost:3000";
 
 const API_BASE_URL = "https://server.vydhyo.com";
 
